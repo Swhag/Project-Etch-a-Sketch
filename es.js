f6879ca68056f50e0a15bac6bd1b77412c694c06
Swhag <khg5293@outlook.com>
2022-12-05 22:00:29 -0500

    code refactor

--- es.js
+++ es.js
@@ -1,24 +1,28 @@
-const defaultColor = "#333333";
-const defaultMode = "color";
+const defaultColor = '#333333';
+const defaultMode = 'color';
 let currentColor = defaultColor;
 let currentMode = defaultMode;
 
-const btnColor = document.getElementById("color");
-const btnRandom = document.getElementById("random");
-const btnEraser = document.getElementById("eraser");
-const btn16 = document.getElementById("16");
-const btn32 = document.getElementById("32");
-const btn64 = document.getElementById("64");
-const clear = document.getElementById("clear");
-const board = document.getElementById("board");
+document.getElementById('color').onmousedown = () => (
+  activateBtn(), setCurrentMode('color')
+);
 
-btnColor.onmousedown = () => (activateBtn(), setCurrentMode("color"));
-btnRandom.onmousedown = () => (activateBtn(), setCurrentMode("random"));
-btnEraser.onmousedown = () => (activateBtn(), setCurrentMode("eraser"));
-btn16.onclick = () => (clearGrid(), makeRows(16, 16));
-btn32.onclick = () => (clearGrid(), makeRows(32, 32));
-btn64.onclick = () => (clearGrid(), makeRows(64, 64));
-clear.onclick = () => (clearGrid(), makeRows(32, 32));
+document.getElementById('random').onmousedown = () => (
+  activateBtn(), setCurrentMode('random')
+);
+
+document.getElementById('eraser').onmousedown = () => (
+  activateBtn(), setCurrentMode('eraser')
+);
+
+document.getElementById('clear').onclick = () => (
+  clearGrid(), makeRows(32, 32)
+);
+
+document.getElementById('16').onclick = () => (clearGrid(), makeRows(16, 16));
+document.getElementById('32').onclick = () => (clearGrid(), makeRows(32, 32));
+document.getElementById('64').onclick = () => (clearGrid(), makeRows(64, 64));
+
 colorPicker.oninput = (e) => setCurrentColor(e.target.value);
 
 let mouseDown = false;
@@ -36,20 +40,22 @@ function setCurrentMode(newMode) {
 }
 
 function makeRows(rows, cols) {
-  board.style.setProperty("--grid-rows", rows);
-  board.style.setProperty("--grid-cols", cols);
+  const board = document.getElementById('board');
+
+  board.style.setProperty('--grid-rows', rows);
+  board.style.setProperty('--grid-cols', cols);
   for (i = 0; i < rows * cols; i++) {
-    const cell = document.createElement("div");
-    cell.classList.add("grid-item");
-    cell.addEventListener("mouseover", changeColor);
-    cell.addEventListener("mousedown", changeColor);
+    const cell = document.createElement('div');
+    cell.classList.add('grid-item');
+    cell.addEventListener('mouseover', changeColor);
+    cell.addEventListener('mousedown', changeColor);
     board.appendChild(cell);
   }
 }
 
 function getRandomColor() {
-  let letters = "0123456789ABCDEF";
-  let color = "#";
+  let letters = '0123456789ABCDEF';
+  let color = '#';
   for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
@@ -57,31 +63,31 @@ function getRandomColor() {
 }
 
 function changeColor(e) {
-  if (e.type === "mouseover" && !mouseDown) return;
-  if (currentMode === "random") {
+  if (e.type === 'mouseover' && !mouseDown) return;
+  if (currentMode === 'random') {
     e.target.style.backgroundColor = getRandomColor();
-  } else if (currentMode === "color") {
+  } else if (currentMode === 'color') {
     e.target.style.backgroundColor = currentColor;
-  } else if (currentMode === "eraser") {
-    e.target.style.backgroundColor = "white";
+  } else if (currentMode === 'eraser') {
+    e.target.style.backgroundColor = 'white';
   }
 }
 
 function activateBtn() {
-  const btns = document.getElementsByClassName("colors");
+  const btns = document.getElementsByClassName('colors');
   for (let i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function () {
-      let current = document.getElementsByClassName("active");
+    btns[i].addEventListener('click', function () {
+      let current = document.getElementsByClassName('active');
       if (current.length > 0) {
-        current[0].className = current[0].className.replace(" active", "");
+        current[0].className = current[0].className.replace(' active', '');
       }
-      this.className += " active";
+      this.className += ' active';
     });
   }
 }
 
 function clearGrid() {
-  board.innerHTML = "";
+  document.getElementById('board').innerHTML = '';
 }
 
 // --------------------------------------------------------------------
